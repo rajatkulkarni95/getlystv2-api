c59f1af849e5b8333f2c0e9a17d53bd2294d5b1e
Rajat Kulkarni <rajatkulkarni95@gmail.com>
2021-08-07 16:05:59 +0530

    fastify-cli generated boilerplate added

---
+++ src/controllers/authentication.ts
@@ -0,0 +1,5 @@
+import { RequestBodyDefault } from "fastify";
+
+export const login = async (request?: RequestBodyDefault) => {
+  return "Login route!\n";
+};
--- src/queries/Playlist/index.ts
+++ src/queries/Playlist/index.ts
@@ -1,5 +1,5 @@
 import prisma from "../../../prisma";
-import { Playlist, PlaylistDetails } from "../../types/playlist";
+import { Playlist, PlaylistDetails } from "../../@types/playlist";
 
 export const createPlaylist = async (
   playlistData: Playlist,
